19146181f4c8b04ff4f69f47d13585c7ef314ba3
Rahul Rajput <rajputrahul3424@gmail.com>
2025-08-11 10:46:51 -0400

    fix: standardize code formatting in cartRoute.js for improved readability

--- Backend/routes/cartRoute.js
+++ Backend/routes/cartRoute.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import { addToCart, getUserCart, updateCart} from '../controllers/cartController.js';
-import authUser from '../middleware/auth.js';
+import express from "express";
+import {
+  addToCart,
+  getUserCart,
+  updateCart,
+} from "../controllers/cartController.js";
+import authUser from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
-// If someone click the endpoint that time the token will be varified 
-cartRouter.post('/get', authUser, getUserCart);
-cartRouter.post('/add', authUser, addToCart);
-cartRouter.post('/update', authUser, updateCart);
+// If someone click the endpoint that time the token will be varified
+cartRouter.post("/get", authUser, getUserCart);
+cartRouter.post("/add", authUser, addToCart);
+cartRouter.post("/update", authUser, updateCart);
 
 export default cartRouter;
-
- 
